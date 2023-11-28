cf80c5e2948ecb2a9fb28b58e28edbbe157efbd8
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-11-28 14:44:00 -0500

    set up sass

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>Where in the world?</h1>
+            {/* Dark Mode toggle */}
+        </header>
+    )
+}
+
+export default Header
---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import '../styles/styles.scss';
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <div>Home Page</div>
+        </>
+    )
+}
+
+export default Home
