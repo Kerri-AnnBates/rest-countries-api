76b20ca3031889a093590558ba8832a1cd27189f
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-11-29 15:47:44 -0500

    add dynamic icon to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon as solidMoon } from '@fortawesome/free-solid-svg-icons';
 import { faMoon as regularMoon } from '@fortawesome/free-regular-svg-icons';
 
 function Header() {
+    const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+    useEffect(() => {
+        const isDark = localStorage.getItem('isDark');
+        if (isDark != null) {
+            setIsDarkMode(isDark === 'true');
+        }
+    }, []);
+
+
+    const handleDarkModeToggle = () => {
+        const isDark = !isDarkMode;
+        localStorage.setItem('isDark', isDark.toString());
+        setIsDarkMode(isDark);
+    }
+
+
     return (
         <header>
             <div className='container'>
                 <div className='flex-header'>
                     <h1>Where in the world?</h1>
-                    <div><FontAwesomeIcon icon={regularMoon} /> Dark Mode</div>
+                    <button onClick={handleDarkModeToggle}>
+                        {isDarkMode ? <FontAwesomeIcon icon={solidMoon} /> : <FontAwesomeIcon icon={regularMoon} />} Dark Mode
+                    </button>
                 </div>
             </div>
         </header>
