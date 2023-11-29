e7385902518e74cc07938a78b84a3c8d18a110c7
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-11-29 11:09:02 -0500

    add icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon as solidMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon as regularMoon } from '@fortawesome/free-regular-svg-icons';
 
 function Header() {
     return (
         <header>
             <h1>Where in the world?</h1>
-            {/* Dark Mode toggle */}
+            <div><FontAwesomeIcon icon={regularMoon} /> Dark Mode</div>
         </header>
     )
 }
