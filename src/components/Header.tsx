a21c306358a1aec0c9735da04394109d0b09bdb7
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-11-30 11:22:46 -0500

    update icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoon as solidMoon } from '@fortawesome/free-solid-svg-icons';
-import { faMoon as regularMoon } from '@fortawesome/free-regular-svg-icons';
+import NightlightOutlinedIcon from '@mui/icons-material/NightlightOutlined';
+import NightlightIcon from '@mui/icons-material/Nightlight';
 
 function Header() {
     const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
@@ -27,7 +26,7 @@ function Header() {
                 <div className='flex-header'>
                     <h1>Where in the world?</h1>
                     <button onClick={handleDarkModeToggle}>
-                        {isDarkMode ? <FontAwesomeIcon icon={solidMoon} /> : <FontAwesomeIcon icon={regularMoon} />} Dark Mode
+                        {isDarkMode ? <NightlightIcon sx={{ fontSize: 16 }} /> : <NightlightOutlinedIcon sx={{ fontSize: 16 }} />} Dark Mode
                     </button>
                 </div>
             </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,10 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar() {
     return (
         <div className='search-container'>
-            <FontAwesomeIcon icon={faMagnifyingGlass} className='search-icon' />
+            <SearchIcon className='search-icon' />
             <input type="search" name="search" id="search" placeholder='Search for a country...' />
         </div>
     )
