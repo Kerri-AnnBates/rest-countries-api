8c9a9deec93410d764aa6ebc5de2321defa2f874
Kerri-Ann Bates <kaybee1jam@yahoo.com>
2023-11-30 10:40:09 -0500

    create search bar and filter dropdown

--- src/components/FilterByRegion.tsx
+++ src/components/FilterByRegion.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import { useState } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 function FilterByRegion() {
+    const [age, setAge] = useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setAge(event.target.value);
+    };
+
     return (
-        <div>
-            <select name="filter" id="filter">
-                <option value="">Filter by Region</option>
-                <option value="1">Africa</option>
-                <option value="2">America</option>
-            </select>
-        </div>
+        <FormControl className='filter-container'>
+            <Select
+                className='dropdown'
+                value={age}
+                onChange={handleChange}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+            >
+                <MenuItem value="">Filter by Region</MenuItem>
+                <MenuItem value={10}>Africa</MenuItem>
+                <MenuItem value={20}>America</MenuItem>
+                <MenuItem value={30}>Asia</MenuItem>
+            </Select>
+        </FormControl>
+
     )
 }
 
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function SearchBar() {
     return (
-        <div>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <div className='search-container'>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className='search-icon' />
             <input type="search" name="search" id="search" placeholder='Search for a country...' />
         </div>
     )
